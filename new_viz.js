e145e107c11355544ac4ce8adb1d6e9d3b9839e6
Peter Ball <peterball12@gmail.com>
2020-05-10 16:21:34 -0300

    Improved basic viz

--- new_viz.js
+++ new_viz.js
@@ -32,19 +32,80 @@ window.onload = function() {
     var WIDTH = canvas.width;
     var HEIGHT = canvas.height;
 
+    var radii_low = [];
+    var radii_lowmid = [];
+    var radii_mid = [];
+    var radii_midhigh = [];
+    var radii_high = [];
+    for (var i = 0; i < 5; i++) {
+      radii_low.push(0);
+      radii_lowmid.push(0);
+      radii_mid.push(0);
+      radii_midhigh.push(0);
+      radii_high.push(0);
+    }
+
+    circle_count = 0
     function renderFrame() {
+      ctx.clearRect(0, 0, WIDTH, HEIGHT);
       requestAnimationFrame(renderFrame);
 
       analyser.getByteFrequencyData(dataArray);
 
-      var avg_freq = dataArray.reduce((previous, current) => current += previous) / dataArray.length;
+      
 
       ctx.fillStyle = "#000";
       ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-      ctx.arc(WIDTH/2, HEIGHT/2, avg_freq, 0, 2 * Math.PI);
-      ctx.strokeStyle = "red";
-      ctx.stroke();
+      radii_low[circle_count] = dataArray.slice(0, bufferLength/5).reduce((previous, current) => current += previous) / dataArray.length;
+      radii_lowmid[circle_count] = dataArray.slice(bufferLength/5, 2*bufferLength/5).reduce((previous, current) => current += previous) / dataArray.length;
+      radii_mid[circle_count] = dataArray.slice(2*bufferLength/5, 3*bufferLength/5).reduce((previous, current) => current += previous) / dataArray.length;
+      radii_midhigh[circle_count] = dataArray.slice(3*bufferLength/5, 4*bufferLength/5).reduce((previous, current) => current += previous) / dataArray.length;
+      radii_high[circle_count] = dataArray.slice(4*bufferLength/5, bufferLength).reduce((previous, current) => current += previous) / dataArray.length;
+
+      for (var i = 0; i < 5; i++) {
+        //Draw low
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.arc(WIDTH/2, HEIGHT/2, 5*radii_low[i], 0, 2 *Math.PI);
+        ctx.strokeStyle = "blue";
+        ctx.stroke();
+        ctx.closePath();
+
+        //Draw lowmid
+        ctx.beginPath();
+        ctx.arc(WIDTH/2, HEIGHT/2, 5*radii_lowmid[i], 0, 2 *Math.PI);
+        ctx.strokeStyle = "purple";
+        ctx.stroke();
+        ctx.closePath();
+
+        //Draw mid
+        ctx.beginPath();
+        ctx.arc(WIDTH/2, HEIGHT/2, 5*radii_mid[i], 0, 2 *Math.PI);
+        ctx.strokeStyle = "white";
+        ctx.stroke();
+        ctx.closePath();
+
+        //Draw midhigh
+        ctx.beginPath();
+        ctx.arc(WIDTH/2, HEIGHT/2, 5*radii_midhigh[i], 0, 2 *Math.PI);
+        ctx.strokeStyle = "pink";
+        ctx.stroke();
+        ctx.closePath();
+
+        //Draw high
+        ctx.beginPath();
+        ctx.arc(WIDTH/2, HEIGHT/2, 5*radii_high[i], 0, 2 *Math.PI);
+        ctx.strokeStyle = "red";
+        ctx.stroke();
+        ctx.closePath();
+      }
+
+      circle_count += 1;
+      if (circle_count == 5){
+        circle_count = 0;
+      }
+
     }
 
     audio.play();
